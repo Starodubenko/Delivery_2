eedbffb3f1408385c340e66cb5bba96c474e318b
Rody <Rody10@mail.ru>
2014-09-03 23:47:04 +0600

    add sql script, maven properties

--- src/main/java/com/epam/star/dao/DaoComand.java
+++ src/main/java/com/epam/star/dao/DaoComand.java
@@ -1,6 +1,6 @@
 package com.epam.star.dao;
 
-import com.epam.star.dao.h2dao.DaoManager;
+import com.epam.star.dao.H2dao.DaoManager;
 
 public interface DaoComand {
     public Object execute(DaoManager daoManager);
